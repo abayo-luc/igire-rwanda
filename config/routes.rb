9182f0b2b01959aafc9f0635f1a8c21520e13729
Jean Luc Abayo <luc.bayo@gmail.com>
2018-01-23 00:45:29 +0200

    added team page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
   get 'contact', to: 'pages#contact'
+  get 'team', to: 'pages#team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
